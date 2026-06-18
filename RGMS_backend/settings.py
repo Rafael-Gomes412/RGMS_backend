6fdfb0645519dffe1bf87e3071d95ca166a2a4f3
Rafael-Gomes412 <rafaelgomesm@outlook.fr>
2026-06-18 16:41:58 +0200

    STATICFILES_STORAGE replacement

--- RGMS_backend/settings.py
+++ RGMS_backend/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'

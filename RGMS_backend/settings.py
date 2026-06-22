4267f2be1f91ea698ae8bda39a4bbb6e027566db
Rafael-Gomes412 <rafaelgomesm@outlook.fr>
2026-06-22 14:11:14 +0200

    kjnb

--- RGMS_backend/settings.py
+++ RGMS_backend/settings.py
@@ -28,13 +28,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',           # ← AVANT staticfiles !
+    
+    # Priorité absolue à staticfiles pour WhiteNoise (gère l'administration CSS)
     'django.contrib.staticfiles',
+    
+    # Stockage persistant Cloudinary pour tes images médias
+    'cloudinary_storage',
     'cloudinary',
+    
+    # Librairies tierces & API
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    
+    # Applications internes RGMS
     'products',
     'orders',
     'users',
